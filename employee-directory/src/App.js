576702328209ef45d27130c9573db1429443706c
Bob Smith <bob@test.com>
2023-07-12 16:45:34 -0400

    navbar and routes

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route, Link} from 'react-router-dom';
+import Home from './Home';
+import EmployeePage from './EmployeePage';
+import SalaryPrediction from './SalaryPrediction';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello There</p>
+      <nav>
+          <div>
+            <Link to="/">Search</Link>
+            <Link to="/SalaryPrediction">Salary Predictor</Link>
+          </div>
+      </nav>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/employee/:id" element={<EmployeePage />} />
+        <Route path="/SalaryPrediction" element={<SalaryPrediction />} />
+      </Routes>
     </div>
   );
 }
---
+++ employee-directory/src/SalaryPrediction.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SalaryPrediction = () => {
+  return (
+    <div>SalaryPrediction</div>
+  )
+}
+
+export default SalaryPrediction
