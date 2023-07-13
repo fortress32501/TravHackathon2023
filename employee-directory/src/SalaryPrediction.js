0e9eb2c353455d6cd1bb94d9fdbfadff4b4f43a1
Bob Smith <bob@test.com>
2023-07-13 13:59:00 -0400

    initial salaryprediction

--- employee-directory/src/SalaryPrediction.js
+++ employee-directory/src/SalaryPrediction.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SalaryPrediction = () => {
+  const [location, setLocation] = useState("");
+  const [job_role, setJobRole] = useState("");
+  const [years_of_experience, setYearsOfExp] = useState(0);
+  const [result, setResult] = useState("");
+
+ 
+
+  const sendData = () => {
+    setResult("")
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ location: location, job_role: job_role, years_of_experience: years_of_experience})
+    };
+    fetch(`http://localhost:4000/predict`, requestOptions)
+        .then((res) => res.json())
+        .then(result => setResult(result));
+  };
+
   return (
-    <div>SalaryPrediction</div>
-  )
+    <>
+    <label>
+      Location:
+    </label>
+    <input type="text" value={location} onChange={e => setLocation(e.target.value)}/>
+    <label>
+      Job Role:
+    </label>
+    <input type="text" value={job_role} onChange={e => setJobRole(e.target.value)}/>
+    <label>  
+      Years of Experience:
+    </label>
+    <input type="text" value={years_of_experience} onChange={e => setYearsOfExp(e.target.value)}/>
+    <button onClick={e => sendData()}>Submit</button>     
+    <p>Result: {result}</p>
+    </>
+  );
 }
 
 export default SalaryPrediction
