09b8a353ecd97f74361bcc63ba4e681fab339876
Bob Smith <bob@test.com>
2023-07-13 16:47:40 -0400

    filter location and job

--- employee-directory/src/Home.js
+++ employee-directory/src/Home.js
@@ -6,6 +6,8 @@ const Home = (props) => {
   const [employees, setEmployees] = useState([]);
   const [first_name, setFirstName] = useState("")
   const [last_name, setLastName] = useState("")
+  const [location, setLocation] = useState("")
+  const [job, setJob] = useState("")
 
   const navigate = useNavigate()
 
@@ -51,11 +53,39 @@ const Home = (props) => {
         onChange={(e)=>setLastName(e.target.value)}
       />
     </div>
+    <div>
+      <select name="locations" id="locations" value = {location} onChange={(e)=>setLocation(e.target.value)}> 
+          <option value="">-</option> 
+          <option value="Des Moines, IA">Des Moines, IA</option> 
+          <option value="Houston, TX">Houston, TX</option> 
+          <option value="Los Angeles, CA">Los Angeles, CA</option> 
+          <option value="Miami, FL">Miami, FL</option>
+          <option value="Minneapolis, MN">Minneapolis, MN</option>
+          <option value="Nashville, TN">Nashville, TN</option>
+          <option value="New York, NY">New York, NY</option>
+          <option value="Seattle, WA">Seattle, WA</option>
+          <option value="Miami, FL">Miami, FL</option> 
+      </select>
+    </div>
+    <div>
+      <select name="jobs" id="jobs" value = {job} onChange={(e)=>setJob(e.target.value)}> 
+          <option value="">-</option> 
+          <option value="accounting">Accounting</option> 
+          <option value="cybersecurity">Cybersecurity</option> 
+          <option value="data_engineer">Data Engineer</option> 
+          <option value="human_resources">Human Resources</option>
+          <option value="manager">Manager</option>
+          <option value="sales">Sales</option>
+          <option value="software_engineer">Software Engineer</option>
+      </select>
+    </div>
     <div>{
       employees.filter(
         employee => 
         employee.first_name.toLowerCase().includes(first_name.toLowerCase()) 
         && employee.last_name.toLowerCase().includes(last_name.toLowerCase())
+        && employee.location.toLowerCase().includes(location.toLowerCase())
+        && employee.job_role.toLowerCase().includes(job.toLowerCase())
       ).map(
         fil_emp => {return <Employee first_name={fil_emp.first_name} last_name={fil_emp.last_name} key={fil_emp.employee_id} id={fil_emp.employee_id} />}
       )}
