cfbaebfccdbb4e798c01d0b0a444528fab05a2af
Bob Smith <bob@test.com>
2023-07-14 15:26:45 -0400

    bootstrap stuff

--- employee-directory/src/SalaryPrediction.js
+++ employee-directory/src/SalaryPrediction.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const SalaryPrediction = () => {
   const [location, setLocation] = useState("");
   const [job_role, setJobRole] = useState("");
-  const [years_of_experience, setYearsOfExp] = useState(0);
+  const [years_of_experience, setYearsOfExp] = useState(1);
   const [result, setResult] = useState("");
 
   let myformat = new Intl.NumberFormat('en-US', { 
@@ -11,8 +11,9 @@ const SalaryPrediction = () => {
     maximumFractionDigits: 2 
   });
  
-  const sendData = () => {
-    setResult("")
+  const sendData = (e) => {
+    e.preventDefault()
+    setResult("-1")
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -25,20 +26,46 @@ const SalaryPrediction = () => {
 
   return (
     <>
-    <label>
-      Location:
-    </label>
-    <input type="text" value={location} onChange={e => setLocation(e.target.value)}/><br/>
-    <label>
-      Job Role:
-    </label>
-    <input type="text" value={job_role} onChange={e => setJobRole(e.target.value)}/><br/>
-    <label>  
-      Years of Experience:
-    </label>
-    <input type="text" value={years_of_experience} onChange={e => setYearsOfExp(e.target.value)}/><br/>
-    <button onClick={e => sendData()}>Submit</button>     
-    <p>Result: ${myformat.format(result)}</p>
+    <form className="row g-3 justify-content-center" onSubmit={e => sendData(e)}>
+    <div className="col-md-4"></div>
+    <div className="col-md-3">
+      <label htmlFor='locations' className="form-label"> Location: </label>
+      <select name="locations" className="form-select" id="locations" value = {location} onChange={(e)=>setLocation(e.target.value)}> 
+            <option value="Des Moines, IA">Des Moines, IA</option> 
+            <option value="Houston, TX">Houston, TX</option> 
+            <option value="Los Angeles, CA">Los Angeles, CA</option> 
+            <option value="Miami, FL">Miami, FL</option>
+            <option value="Minneapolis, MN">Minneapolis, MN</option>
+            <option value="Nashville, TN">Nashville, TN</option>
+            <option value="New York, NY">New York, NY</option>
+            <option value="Seattle, WA">Seattle, WA</option>
+            <option value="Miami, FL">Miami, FL</option> 
+        </select>
+    </div>
+    <div className="col-md-4"></div>
+    <div className="col-md-4"></div>
+    <div className="col-md-3">
+      <label htmlFor="jobs" className="form-label"> Job Role: </label>
+      <select name="jobs" id="jobs" className="form-select" value = {job_role} onChange={e => setJobRole(e.target.value)}> 
+        <option value="accounting">Accounting</option> 
+        <option value="cybersecurity">Cybersecurity</option> 
+        <option value="data_engineer">Data Engineer</option> 
+        <option value="human_resources">Human Resources</option>
+        <option value="manager">Manager</option>
+        <option value="sales">Sales</option>
+        <option value="software_engineer">Software Engineer</option>
+      </select>
+    </div>
+    <div className="col-md-4"></div>
+    <div className="col-md-4"></div>
+    <div className="col-md-3">
+      <label htmlFor="years" className="form-label"> Years of Experience: </label>
+      <input type="number" min="1" id="years" value={years_of_experience}  className="form-control" onChange={e => setYearsOfExp(e.target.value)}/><br/>
+    </div>
+    <div className="col-md-4"></div>
+    <button className="btn btn-primary" style={{"width": "125px"}} type="submit">Submit</button>     
+    <div>{result==="-1" ? <div className="spinner-border m-5" role="status"><span className="visually-hidden">Loading...</span></div>: <h5>Result: ${myformat.format(result)}</h5>}</div>
+    </form>
     </>
   );
 }
